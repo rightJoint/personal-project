fce56ec5ee0bfeb09cee0278a47790f94a2a8e55
Alexey <rightjoint@yandex.ru>
2026-08-03 15:57:57 +0300

    set-up-hosting-stab

---
+++ src/Views/JointSite/Deploy/TpView_JointSite_Deploy_Hosting.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Src\Views\JointSite\Deploy;
+
+
+
+use JointApp\Views\TpView;
+
+
+class TpView_JointSite_Deploy_Hosting extends TpView
+{
+    public function renderView():string
+    {
+        return
+            '<article class="pageContentJointSite">'.
+            '<section>'.
+            '<h3>'.$this->langFile::H3.'</h3>'.
+            '</section>'.
+            '</article>';
+    }
+
+    public static function getCss():array
+    {
+        return [
+            'pageContentJointSite' => '/css/jointSite/pageContentJointSite.css',
+            'codesnippet' => '/css/code-snippet.css',
+        ];
+    }
+
+    public static function loadViewLang(string $lang = 'ru')
+    {
+        $class_Name = 'Src\LangFiles\Views\JointSite\Deploy\LangFiles_'.self::ucfirstLang($lang).'_'.'Views_JS_D_H_Article';
+        $langFile = new $class_Name();
+        return $langFile;
+    }
+}
--- tests/Views/Src_Views_JointSite_Test.php
+++ tests/Views/Src_Views_JointSite_Test.php
@@ -19,6 +19,7 @@ class Src_Views_JointSite_Test extends PHPUnit\Framework\TestCase
 
           'Src\Views\JointSite\Deploy\SiteView_JointSite_Deploy',
           'Src\Views\JointSite\Deploy\SiteView_JointSite_Deploy_OpenServer',
+          'Src\Views\JointSite\Deploy\SiteView_JointSite_Deploy_Hosting',
           'Src\Views\JointSite\Design\View\SiteView_JointSite_D_V_TpView',
           'Src\Views\JointSite\Design\View\SiteView_JointSite_D_V_WebView',
           'Src\Views\JointSite\Design\View\SiteView_JointSite_D_V_SiteView',
@@ -46,6 +47,7 @@ class Src_Views_JointSite_Test extends PHPUnit\Framework\TestCase
 
             'Src\Views\JointSite\Deploy\TpView_JointSite_Deploy_Art',
             'Src\Views\JointSite\Deploy\TpView_JointSite_Deploy_OpenServer',
+            'Src\Views\JointSite\Deploy\TpView_JointSite_Deploy_Hosting',
             'Src\Views\JointSite\Design\View\TpView_JointSite_D_V_TpView',
             'Src\Views\JointSite\Design\View\TpView_JointSite_D_V_WebView',
             'Src\Views\JointSite\Design\View\TpView_JointSite_D_V_SiteView',

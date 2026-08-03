4352fb406c67077f7c82575a6a174d9cdf55170a
Alexey <rightjoint@yandex.ru>
2026-08-03 16:09:04 +0300

    migrations-stab

---
+++ src/Views/JointSite/Deploy/SiteView_JointSite_Deploy_Migrations.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Src\Views\JointSite\Deploy;
+
+
+
+use Src\Views\JointSite\SiteView_JointSite;
+
+class SiteView_JointSite_Deploy_Migrations extends SiteView_JointSite
+{
+
+    protected function replaceDefaultHeadLang()
+    {
+        $class_Name = 'Src\LangFiles\Views\JointSite\Deploy\LangFiles_'.$this->ucfirstLang($this->userLang).'_'.'Views_JS_D_M_Head';
+        return new $class_Name();
+    }
+
+    protected function replaceDefaultHeaderLang()
+    {
+        $class_Name = 'Src\LangFiles\Views\JointSite\Deploy\LangFiles_'.$this->ucfirstLang($this->userLang).'_'.'Views_JS_D_M_Header';
+        return new $class_Name();
+    }
+
+    protected function putArticleTemplate()
+    {
+        $this->tpSet->Migrations = new TpView_JointSite_Deploy_Migrations();
+    }
+
+    protected function handleTpMigrations(): string
+    {
+        return '<div class="contentBlock-frame"><div class="contentBlock-center">'.
+            '<div class="contentBlock-wrap">'.$this->tpSet->Migrations->renderView().
+            '</div></div></div>';
+    }
+}

29e91951162a4e5850d7effd7d8dcbebb3d02edd
Alexey <rightjoint@yandex.ru>
2026-08-06 08:36:16 +0300

    controller

---
+++ JointApp/Controllers/Controller.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace JointApp\Controllers;
+
+
+use JointApp\JointSiteLogger;
+use JointApp\JointSiteUser;
+use Psr\Log\LoggerAwareTrait;
+
+class Controller
+{
+    use LoggerAwareTrait;
+
+    public $model;
+    public $view;
+
+    private $context = ['controller' => __CLASS__];
+
+    protected $langFile;
+
+    public string $userLang = 'ru';
+
+    protected JointSiteUser $user;
+
+    function __construct(JointSiteUser $user, JointSiteLogger &$logger)
+    {
+        $this->setLogger($logger);
+        $this->user = $user;
+        $this->userLang = $user->userLang;
+        $this->setUpLangFile();
+        if(!$this->checkAccessController()){
+            $this->logger->warning('denied in checkAccessController', $this->context);
+        }
+    }
+
+    protected function checkAccessController():bool
+    {
+        return true;
+    }
+
+    protected function setUpLangFile()
+    {
+        $this->langFile = new \stdClass();
+    }
+
+    public function actionIndex()
+    {
+        $this->model->getData();
+    }
+}

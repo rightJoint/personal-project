e23aad1191ab4b585467ea5ede21a9534c75d4c0
Alexey <rightjoint@yandex.ru>
2026-08-10 21:33:29 +0300

    test-list-detail-view

---
+++ src/Views/Test/Records/SiteView_Test_Records_ListView.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Src\Views\Test\Records;
+
+
+use JointApp\Views\Records\RecordListView;
+use Src\Views\Test\TpView_Test_Menu;
+
+class SiteView_Test_Records_ListView extends RecordListView
+{
+    public array $tblList = [];
+
+    protected function putCustomTemplates():void
+    {
+        $this->tpSet->TestMenu = new TpView_Test_Menu();
+        $this->tpSet->TS = new TpView_Test_Records_TblSelector();
+        parent::putCustomTemplates();
+    }
+
+    protected function handleTpTestMenu(): string
+    {
+        return '<div class="contentBlock-frame"><div class="contentBlock-center">'.
+            '<div class="contentBlock-wrap">'.$this->tpSet->TestMenu->renderView().
+            '</div></div></div>';
+    }
+
+    protected function handleTpTS(): string
+    {
+        return '<div class="contentBlock-frame"><div class="contentBlock-center">'.
+            '<div class="contentBlock-wrap">'.$this->tpSet->TS->renderView().
+            '</div></div></div>';
+    }
+
+}

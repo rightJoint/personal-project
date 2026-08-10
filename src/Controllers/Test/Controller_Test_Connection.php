5e5013d981a82e22ede35b0aaa4dc49142e85ec1
Alexey <rightjoint@yandex.ru>
2026-08-10 18:28:38 +0300

    test-connection

---
+++ src/Controllers/Test/Controller_Test_Connection.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Src\Controllers\Test;
+
+
+use JointApp\Models\Model_Pdo;
+use JointApp\SettingsDb;
+
+
+class Controller_Test_Connection extends Controller_Test
+{
+
+    public bool $serverConnectStatus = true;
+    public bool $dbConnectStatus = false;
+    public string $reason = '';
+    public string $result = '';
+    public string $create_log = '';
+
+    public function actionIndex()
+    {
+        $model_pdo = new Model_Pdo($this->user, $this->logger);
+        $this->serverConnectStatus = $model_pdo->getServerStatus();
+        $this->dbConnectStatus = $model_pdo->getDbStatus();
+        $this->reason = $model_pdo->getLogMessage();
+
+        $this->updateViewParams($this->view);
+    }
+
+    public function actionCreateDatabase()
+    {
+        $pdo = new \PDO('mysql:host=' . SettingsDb::DB_HOST. ';',
+            SettingsDb::DB_USER, SettingsDb::DB_PW);
+        try{
+            $pdo->query('CREATE DATABASE '.SettingsDb::DB_NAME.' CHARACTER SET utf8 COLLATE utf8_general_ci', \PDO::FETCH_ASSOC);
+        }catch (\Exception $e) {
+            $this->create_log = $e->getMessage();
+        }
+        $model_pdo = new Model_Pdo($this->user, $this->logger);
+        $this->serverConnectStatus = $model_pdo->getServerStatus();
+        $this->dbConnectStatus = $model_pdo->getDbStatus();
+        $this->reason = $model_pdo->getLogMessage();
+
+        $this->updateViewParams($this->view);
+    }
+}

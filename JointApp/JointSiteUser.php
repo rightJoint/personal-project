17300eddf6ad13a75489dfb531f38cc7329bd064
Alexey <rightjoint@yandex.ru>
2026-08-19 16:40:43 +0300

    add user

---
+++ JointApp/JointSiteUser.php
@@ -0,0 +1,150 @@
+<?php
+
+
+namespace JointApp;
+
+
+use JointApp\Models\PdoTrait;
+
+class JointSiteUser
+{
+    use PdoTrait;
+
+    private string $user_id = '';
+    private string $alias = '';
+    private bool $blackList = false;
+    private string $followed_by = '';
+    private string $avatar = '';
+    private string $login = '';
+    private bool $isAuth = false;
+    private bool $isValid = false;
+    private string $validBy = '';
+    private bool $isAdmin = false;
+
+    private string $userLang = 'ru';
+
+    private $context = ['User' => __CLASS__];
+
+    private string $login_tmp = '';
+
+    public function __construct(JointSiteLogger &$logger, string $userLang = 'ru')
+    {
+        $this->logger = $logger;
+        $this->connectDb();
+    }
+
+
+    public function withLogin(string $login):bool
+    {
+        if(self::checkUserLogin($login)){
+            $this->login_tmp = $login;
+            return true;
+        }
+        return false;
+    }
+
+    public function withPassword(string $password):bool
+    {
+        if($this->login_tmp) {
+            if (self::checkUserPassword($password)) {
+                $qry = 'select user_id, alias, pw_hash, avatar, blackList, followed_by from users where user_id = "' . $this->login_tmp . 'qq"';
+                if ($res = $this->pdoQuery($qry)) {
+                    if($res->rowCount() == 1){
+                        $row = $res->fetch(\PDO::FETCH_ASSOC);
+                        if (password_verify($password, $row['pw_hash'])) {
+                            $this->blackList = $row['blackList'];
+                            if(!$this->blackList){
+                                $this->user_id = $row['user_id'];
+                                $this->alias = $row['alias'];
+                                $this->avatar = $row['avatar'];
+                                $this->followed_by = $row['followed_by'];
+                            }
+                            $this->isAuth = true;
+                            $this->saveSession();
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    function quitUser():void
+    {
+        $this->user_id = '';
+        $this->alias = '';
+        $this->blackList = false;
+        $this->followed_by = '';
+        $this->avatar = '';
+        $this->login = '';
+        $this->isAuth = false;
+        $this->isValid = false;
+        $this->validBy = '';
+        $this->isAdmin = false;
+        unset($_SESSION['user']);
+    }
+
+    public function fromSession()
+    {
+        if(isset($_SESSION['user'])){
+            $this->user_id = $_SESSION['user_id'];
+            $this->alias = $_SESSION['alias'];
+            $this->followed_by = $_SESSION['followed_by'];
+            $this->blackList = $_SESSION['blackList'];
+            $this->avatar = $_SESSION['avatar'];
+        }
+    }
+
+    public function saveSession():void
+    {
+        $_SESSION['user'] = [
+            'user_id' => $this->user_id,
+            'alias' => $this->alias,
+            'followed_by' => $this->followed_by,
+            'blackList' => $this->blackList,
+            'avatar' => $this->avatar,
+        ];
+    }
+
+    public static function checkUserLogin($login):bool
+    {
+        if (preg_match('/^[A-Za-z]{1}[0-9a-zA-Z-._]{2,15}$/imsiu', $login) == 0){
+            return false;
+        }
+        return true;
+    }
+
+    public static function checkUserPassword($password):bool
+    {
+        if (strlen($password) >= 3){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static function checkUserEmail($user_email)
+    {
+        if (filter_var($user_email, FILTER_VALIDATE_EMAIL)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function checkDoubleLogin($login):bool
+    {
+        $countLogin_qry = "select count(user_id) as cnt from users where login = '".$login."'";
+        if($res = $this->pdoQuery($countLogin_qry)){
+            if($res->fetch(\PDO::FETCH_ASSOC)["cnt"] == 0){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getUserLang():string
+    {
+        return $this->userLang;
+    }
+}

a650386b5bc018ee4f7966eb304734492e9ddb37
Alexey <rightjoint@yandex.ru>
2026-08-20 12:01:58 +0300

    add-langs

---
+++ JointApp/LangFiles/Views/SiteView/AuthForm/LangFiles_Ru_Views_AuthForm.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace JointApp\LangFiles\Views\SiteView\AuthForm;
+
+
+class LangFiles_Ru_Views_AuthForm
+{
+    const AUTH_SU_TITLE = 'Регистрация на сайте';
+    const AUTH_SU_LOGIN_PS = 'Придумайте логин';
+    const AUTH_SU_PASS_PS = 'пароль';
+    const AUTH_SU_PASSRP_PS = 'повторите пароль';
+    const AUTH_SU_CAP = 'код с картинки';
+    const AUTH_SU_SUBMIT = 'зарегистрировать';
+    const AUTH_SU_SIL = 'Вход на сайт';
+
+    const AUTH_SU_ERR_PASS_MATCH = 'Пароли не совпадают';
+    const AUTH_SU_ERR_LOGIN_ACCEPT = 'Недопустимый логин';
+    const AUTH_SU_ERR_LOGIN_RESERVED = 'Логин зарезервирован';
+    const AUTH_SU_ERR_PASS_ACCEPT = 'Недопустимый пароль';
+    const AUTH_SU_ERR_CAPTCHA_EMPTY = 'Код не введен';
+    const AUTH_SU_ERR_CAPTCHA_WRONG = 'Неправильный код';
+    const AUTH_SU_ERR_UNKNOWN = 'Неизвестная ошибка';
+
+    const AUTH_SI_TITLE = 'Вход на сайт';
+    const AUTH_SI_LOGIN_PS = 'Ваш логин';
+    const AUTH_SI_PASS_PS = 'пароль';
+    const AUTH_SI_ERR_LOGIN_NOTFOUND = 'Логин не найден';
+    const AUTH_SI_ERR_LOGIN_BLL = 'Логин в черном списке';
+    const AUTH_SI_ERR_LOGIN_ACCEPT = 'Недопустимый логин';
+    const AUTH_SI_ERR_PASS_ACCEPT = 'Недопустимый пароль';
+    const AUTH_SI_ERR_WRONG_LP = 'Неправильный логин или пароль';
+    const AUTH_SI_SUBMIT = 'войти';
+    const AUTH_SI_SIL = 'Регистрация на сайте';
+
+}

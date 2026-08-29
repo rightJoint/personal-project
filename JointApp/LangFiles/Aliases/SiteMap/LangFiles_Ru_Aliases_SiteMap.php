5a5ddeb012ff5909f3e90c8aaccbb920a4d26b50
Alexey <rightjoint@yandex.ru>
2026-08-29 17:15:04 +0300

    sitemap aliases prepare

---
+++ JointApp/LangFiles/Aliases/SiteMap/LangFiles_Ru_Aliases_SiteMap.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class LangFiles_Ru_Aliases_SiteMap
+{
+    public $fieldAliases = array(
+        'maploc' => 'uri',
+        'lastmod' => 'дт.изм',
+        'changefreq' => 'частота',
+        'priority' => 'приоритет',
+        'comment' => 'коммент.',
+        'use_flag' => 'исп.',
+        'date_created' => 'дт.созд.',
+        'created_by' => 'создал id',
+    );
+}

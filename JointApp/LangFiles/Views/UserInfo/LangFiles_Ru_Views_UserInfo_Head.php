4f8685ca5d49a00aa8bae5a0e2eb819a90102f49
Alexey <rightjoint@yandex.ru>
2026-09-03 22:10:36 +0300

    user info

---
+++ JointApp/LangFiles/Views/UserInfo/LangFiles_Ru_Views_UserInfo_Head.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace JointApp\LangFiles\Views\UserInfo;
+
+
+class LangFiles_Ru_Views_UserInfo_Head
+{
+    const META_DESCRIPTION = 'Информация о пользователе';
+    const PAGE_TITLE =  'профиль пользователя';
+}
--- JointApp/Router/JointSiteRouteFinder.php
+++ JointApp/Router/JointSiteRouteFinder.php
@@ -8,6 +8,7 @@ namespace JointApp\Router;
 use JointApp\Router\RoutesCollection\PersonalPage\RoutesCollection_PP;
 use JointApp\Router\RoutesCollection\Siteman\RoutesCollection_Siteman;
 use JointApp\Router\RoutesCollection\User\RoutesCollection_User;
+use JointApp\Router\RoutesCollection\UserInfo\RoutesCollection_UserInfo;
 use Psr\Log\LoggerAwareTrait;
 use JointApp\Router\RoutesCollection\Api\RoutesCollection_Api;
 use JointApp\Router\RoutesCollection\RoutesCollection_JointSite;
@@ -26,6 +27,7 @@ class JointSiteRouteFinder
     use RoutesCollection_User;
     use RoutesCollection_Siteman;
     use RoutesCollection_PP;
+    use RoutesCollection_UserInfo;
 
     private $context = ['RouteFinder' => __CLASS__];
 

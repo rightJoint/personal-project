d507e6c4d37f2510e78e8905fc60d5df1640609d
Alexey <rightjoint@yandex.ru>
2026-09-04 10:34:01 +0300

    user info test view

---
+++ tests/Views/JointApp_Views_UserInfo_Test.php
@@ -0,0 +1,49 @@
+<?php
+//php ./vendor/bin/phpunit tests/Views/JointApp_Views_UserInfo_Test.php
+
+
+
+class JointApp_Views_UserInfo_Test extends PHPUnit\Framework\TestCase
+{
+    protected function setUp(): void
+    {
+
+    }
+
+    public function testSiteViews():void
+    {
+        $langSet = ['ru', 'en'];
+
+        $Ns = array(
+            'JointApp\Views\UserInfo\SiteView_UserInfo',
+        );
+
+        for($i=1; $i<=count($langSet); $i++){
+            foreach ($Ns as $class){
+                $SiteView = new $class();
+                $SiteView->userLang = $langSet[$i-1];
+                $SiteView->handleViewParams();
+                $SiteView->setUpCustomLang($SiteView->getDefaultLang());
+                $SiteView->getResponseHtml();
+            }
+        }
+    }
+
+    public function testTemplateViews():void
+    {
+        $langSet = ['ru', 'en'];
+
+        $Ns = array(
+        );
+
+        for($i=1; $i<=count($langSet); $i++){
+            $userLang = $langSet[$i-1];
+            foreach ($Ns as $class){
+                $tpView = new $class();
+                $tpView->handleViewParams();
+                $tpView->setUpCustomLang($tpView->getDefaultLang());
+                $tpView->getResponseHtml();
+            }
+        }
+    }
+}

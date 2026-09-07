116a5f405471b488c637acba438a54b4311b8f8e
Alexey <rightjoint@yandex.ru>
2026-09-07 17:22:12 +0300

    recover blog migration

---
+++ migrations/2026-09-07-recover-blog.sql
@@ -0,0 +1,45 @@
+insert into blogArts (
+art_id, artCat, artRef, artName_en, artName_ru, artMeta_en, artMeta_ru, artImg, activeFlag, indexFlag, pubDate, refreshDate, commentsFlag, popFlag, created_by)
+ values 
+('DFDE4A80-A79D-4A3E-81E3-66FD6A91DB80', '6C20AC2A-7817-4440-A67D-5A3D40471275', 'joint-pass', 'JointPass', 'ДжойнтПасс', 'Passwords organizer. No needs keep in mind all passwords of yours accounts, remember only one Master Pass of this app.', 'Органайзер паролей. Вам не придется помнить пароли от всех ваших учеток, достаточно помнить один МастерПасс от программы.', '801EFDDE-D4BC-48A7-80DF-9C29DF68129F.png', '1', '1', '2024-05-11', '2025-01-04', null, '1', '1AB4C7D7-5315-4C9E-9F33-E1B250491589'),
+('505D46FE-75A1-44B9-9006-5C244C706DDF', '3D56453C-C00E-4241-A2AE-9A6A1993E6A6', 'polygraph-exam', 'polygraph test at work', 'Полиграф на работе', 'Мой опыт прохождения полиграфа. Стоит ли проходить полиграф и какие вопросы задают.', 'Мой опыт прохождения полиграфа. Стоит ли проходить полиграф и какие вопросы задают.', 'AB3B3D55-0ADC-49A0-9771-CD428FE27CA4.png', '1', '1', '2025-01-31', null, null, '1', '1AB4C7D7-5315-4C9E-9F33-E1B250491589'),
+('85CB8498-DF30-435C-9CED-28DDCC018151', '6C20AC2A-7817-4440-A67D-5A3D40471275', 'looking-for-php-fullstack-job-in-2025', 'looking-for-php-fullstack-job-in-2025', 'Поиск работы в it в 2025', 'it-job hunting 2025', 'Поиск удаленной работы разработчиком php в 2025, взгляд со стороны соискателя: обзор вакансий, зарплат, требований работодателей, статистика встреч с hr и технических собеседований', '992B518A-5BCD-4F35-88F5-EE97C7CC2EFB.jpg', '1', '1', '2025-03-18', '2025-05-19', '1', null, '1AB4C7D7-5315-4C9E-9F33-E1B250491589'),
+('BC8BE351-3B0B-4705-9C1A-0BDC8E536F74', '6C20AC2A-7817-4440-A67D-5A3D40471275', 'right-joint-updated', 'rightjoint.ru updated', 'rightjoint.ru обновлен', 'Зачем нужен этот сайт и какие вопросы он решает', 'Зачем нужен этот сайт и какие вопросы он решает', '2C61DEE3-2AB1-4B1C-8BDF-87038DAF043B.png', '1', null, '2025-01-25', null, '1', '1', '1AB4C7D7-5315-4C9E-9F33-E1B250491589'),
+('39346989-166F-4C4B-B62B-A1020C2F0D7D', '6C20AC2A-7817-4440-A67D-5A3D40471275', 'phpstorm-reset-trial', 'Сброс тестового периода Phpstorm на Linux и Windows', 'Сброс тестового периода Phpstorm на Linux и Windows', 'Как сбросить тестовый период Phpstorm на Linux Ubuntu и Windows-10 и восстановить проекты и подключения', 'Как сбросить тестовый период Phpstorm на Linux Ubuntu и Windows-10 и восстановить проекты и подключения', '54BDA560-5BA1-4F09-9741-41EC46E460A8.png', '1', null, '2019-10-21', '2021-03-24', null, '1', '1AB4C7D7-5315-4C9E-9F33-E1B250491589'),
+('EF675FC8-689B-4848-9A4F-A56AD2C531CB', '6C20AC2A-7817-4440-A67D-5A3D40471275', 'test-task-alvasar', 'test-task-alvasar', 'тестовое задание Альвасар', 'тестовое задание из академии нумерологии - расчет кода судьбы', 'тестовое задание из академии нумерологии - расчет кода судьбы', '9BE1CC14-1CF6-4520-BAA5-3F962547E02D.png', '1', null, '2025-03-04', null, null, null, '1AB4C7D7-5315-4C9E-9F33-E1B250491589'),
+('04493255-0496-4713-A043-C904BB20D43A', '6C20AC2A-7817-4440-A67D-5A3D40471275', 'test-task-parse-brackets', 'test-task-parse-brackets', 'тестовое задание - разбор скобок', 'проверка правильности написания скобок в выражении', 'проверка правильности написания скобок в выражении', 'C131AF86-CB06-42E3-B8E8-BBAFD09C44DB.jpg', '1', null, '2025-03-04', null, null, null, '1AB4C7D7-5315-4C9E-9F33-E1B250491589'),
+('5F99E5D4-0DA1-41E6-8253-3FB6AFD602B6', '3D56453C-C00E-4241-A2AE-9A6A1993E6A6', 'censored', 'Цензура в интернет', 'Цензура в интернет', 'Откуда берутся фэйковые новости и насколько достоверны официальные источники, теории заговора и способы избежать цензуры, каким источникам можно доверять и какое будущее ждёт цензуру в интернет', 'Откуда берутся фэйковые новости и насколько достоверны официальные источники, теории заговора и способы избежать цензуры, каким источникам можно доверять и какое будущее ждёт цензуру в интернет', '88A0E808-441F-408F-845C-A6A03E9ECC28.jpg', '1', null, '2025-04-04', null, null, '1', '1AB4C7D7-5315-4C9E-9F33-E1B250491589'),
+('ED217756-64E3-4E38-A794-003EB36BCCA8', '6C20AC2A-7817-4440-A67D-5A3D40471275', 'sql-cheat-sheet', 'SQL-cheat-sheet', 'Шпаргалка по SQL', 'Fifty SQL questions and answers to prepare for the job interview', 'Пятьдесят вопросов и ответов по SQL для подготовки к собеседованию', '3AABF75F-9F27-4E97-BCF8-AEF47AAC51F1.png', '1', null, '2025-04-05', null, null, null, '1AB4C7D7-5315-4C9E-9F33-E1B250491589');
+insert into blogCats (
+    cat_id, catAlias, catName_en, catName_ru, catMeta_en, catMeta_ru, catImg, activeFlag, indexFlag, created_by)
+values
+('3D56453C-C00E-4241-A2AE-9A6A1993E6A6', 'job', 'job', 'работа', 'job-descr', 'работа-описание', '8EE9AEEC-FD6F-4C12-AEAC-9E8540072F1A.jpg', '1', '1', '1AB4C7D7-5315-4C9E-9F33-E1B250491589');
+insert into blogTags (
+    tag_id, tag_en, tag_ru, created_by)
+values
+('52B40FD3-D61F-4F2C-95FA-0CCB526D1602', 'c#', 'c#', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('0F5BEE96-01BE-4FC6-9D8C-D956979FF1B9', 'project', 'проект', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('EBE37C2E-26C5-4CC5-89A9-247FEC55B05D', 'job', 'работа', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('3BB1EC3B-3B25-4DDA-BCBB-E0717540A64D', 'test task', 'тестовое задание', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('81AFE50D-BC87-4695-8045-BB0B70BD491E', 'censored', 'цензура', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('6B51DDC9-B761-4D0B-A716-704155F852BC', 'internet', 'интернет', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('D747CB07-A322-49A3-A0A5-1AF6177A80F4', 'Sql', 'Sql', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E');
+insert into blogAtrTags (
+    art_id, tag_id, created_by)
+values
+('DFDE4A80-A79D-4A3E-81E3-66FD6A91DB80', '52B40FD3-D61F-4F2C-95FA-0CCB526D1602', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('85CB8498-DF30-435C-9CED-28DDCC018151', 'EBE37C2E-26C5-4CC5-89A9-247FEC55B05D', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('39346989-166F-4C4B-B62B-A1020C2F0D7D', '83D91035-8B32-44F9-9648-A5A58CB23C78', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('DFDE4A80-A79D-4A3E-81E3-66FD6A91DB80', '0F5BEE96-01BE-4FC6-9D8C-D956979FF1B9', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('BC8BE351-3B0B-4705-9C1A-0BDC8E536F74', '83D91035-8B32-44F9-9648-A5A58CB23C78', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('BC8BE351-3B0B-4705-9C1A-0BDC8E536F74', '0F5BEE96-01BE-4FC6-9D8C-D956979FF1B9', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('505D46FE-75A1-44B9-9006-5C244C706DDF', 'EBE37C2E-26C5-4CC5-89A9-247FEC55B05D', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('EF675FC8-689B-4848-9A4F-A56AD2C531CB', '83D91035-8B32-44F9-9648-A5A58CB23C78', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('EF675FC8-689B-4848-9A4F-A56AD2C531CB', '3BB1EC3B-3B25-4DDA-BCBB-E0717540A64D', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('04493255-0496-4713-A043-C904BB20D43A', '83D91035-8B32-44F9-9648-A5A58CB23C78', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('04493255-0496-4713-A043-C904BB20D43A', '3BB1EC3B-3B25-4DDA-BCBB-E0717540A64D', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('85CB8498-DF30-435C-9CED-28DDCC018151', '83D91035-8B32-44F9-9648-A5A58CB23C78', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('5F99E5D4-0DA1-41E6-8253-3FB6AFD602B6', '81AFE50D-BC87-4695-8045-BB0B70BD491E', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('5F99E5D4-0DA1-41E6-8253-3FB6AFD602B6', '6B51DDC9-B761-4D0B-A716-704155F852BC', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('ED217756-64E3-4E38-A794-003EB36BCCA8', '0B158415-19FA-41F1-8A4B-8CF8469E6188', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E'),
+('ED217756-64E3-4E38-A794-003EB36BCCA8', 'D747CB07-A322-49A3-A0A5-1AF6177A80F4', 'F42F81F8-1300-41CA-89BB-36BD7417BE1E');

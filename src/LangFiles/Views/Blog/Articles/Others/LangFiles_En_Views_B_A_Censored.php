13927307042ea30c940f228b9f32eef20633d2ff
Alexey <rightjoint@yandex.ru>
2026-09-08 10:42:50 +0300

    prepare article censored

---
+++ src/LangFiles/Views/Blog/Articles/Others/LangFiles_En_Views_B_A_Censored.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Src\LangFiles\Views\Blog\Articles\Others;
+
+
+class LangFiles_En_Views_B_A_Censored
+{
+
+}
--- src/RoutesCollection/Blog/RoutesCollection_Blog.php
+++ src/RoutesCollection/Blog/RoutesCollection_Blog.php
@@ -43,6 +43,8 @@ trait RoutesCollection_Blog
                     $route->withView('Src\Views\Blog\Others\SiteView_Blog_Others_RightJointUpdated');
                 }elseif(strtolower($routes_ns[3]) == 'looking-for-php-fullstack-job-in-2025'){
                     $route->withView('Src\Views\Blog\Job\SiteView_Blog_Job_PhpJob2025');
+                }elseif(strtolower($routes_ns[3]) == 'censored'){
+                    $route->withView('Src\Views\Blog\Others\SiteView_Blog_Others_Censored');
                 }
                 else{
                     $route->withView('Src\Views\Blog\SiteView_Blog_Art');

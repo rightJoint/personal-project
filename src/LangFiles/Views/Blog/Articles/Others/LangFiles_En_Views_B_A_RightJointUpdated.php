4b46afa5065ceb112ad92fa283c5006111f399a0
Alexey <rightjoint@yandex.ru>
2026-09-09 10:17:19 +0300

    review article rightjoint updated

--- src/LangFiles/Views/Blog/Articles/Others/LangFiles_En_Views_B_A_RightJointUpdated.php
+++ src/LangFiles/Views/Blog/Articles/Others/LangFiles_En_Views_B_A_RightJointUpdated.php
@@ -5,5 +5,24 @@ namespace Src\LangFiles\Views\Blog\Articles\Others;
 
 class LangFiles_En_Views_B_A_RightJointUpdated
 {
-
+    const S1_P1 = 'I started this project over 10 years ago, in 2015, and have been working on it ever since. '.
+    'The reasons why I keep moving forward largely remain the same over time';
+    const S1_P1_LI1 = 'I love programming and I love PHP, so I periodically check the code and update the repository';
+    const S1_P1_LI2 = 'I don"t like social media because I think here I can do things better than there, and I prefer '.
+    'to keep the information I need on my own resource where I set my own rules.';
+    const S1_P1_LI3='Perhaps my project will help me find a decent job or at least avoid the need to prove my experience. '.
+    'The code of the project is on the pub repository where are few examples of test tasks that I have already done'.
+    'The project code is located in a public repository, where you can also find several examples of test tasks I have already completed.';
+    const S1_P1_LI4='To assemble a team of real users, like-minded people with whom I find it interesting to communicate and spend time';
+    const S1_P2='So, first of all, I need this site for myself, because it gives me opportunities for self-development and self-expression.';
+    const S2_P1='Reasons why this site is useful to others.';
+    const S2_P1_LI1='I publish materials on various topics. Perhaps someone will find something interesting there';
+    const S2_P1_LI2='I feel like I do my job well, so it might be of interest to my clients who appreciate the quality '.
+    'and professionalism I demonstrate here.';
+    const RJ_WARNING='Please note: all information posted on this website is created solely for entertainment purposes '.
+    'by the owner (developer) of this resource and does not encourage anyone to take any action. Some texts and '.
+    'comments may contain language that you may find rude or inappropriate. Access to such material may require the user"s consent.';
+    const PR_TEXT_BEFORE='See also';
+    const PR_REF_TEXT='Privacy Policy';
+    const PR_REF_TITLE='View privacy policy';
 }
--- src/Views/Blog/Others/TpView_Blog_Others_RightJointUpdated.php
+++ src/Views/Blog/Others/TpView_Blog_Others_RightJointUpdated.php
@@ -13,4 +13,36 @@ class TpView_Blog_Others_RightJointUpdated extends TpView
         $class_Name = 'Src\LangFiles\Views\Blog\Articles\Others\LangFiles_'.self::ucfirstLang($this->userLang).'_Views_B_A_RightJointUpdated';
         return new $class_Name();
     }
+
+    public function getResponseHtml(): string
+    {
+        return '<section>'.
+            '<p>'.$this->langFile::S1_P1.':'.
+            '<ul>'.
+            '<li>'.$this->langFile::S1_P1_LI1.'</li>'.
+            '<li>'.$this->langFile::S1_P1_LI2.'</li>'.
+            '<li>'.$this->langFile::S1_P1_LI3.'</li>'.
+            '<li>'.$this->langFile::S1_P1_LI4.'</li>'.
+            '</ul>'.
+            '</p>'.
+            '<p>'.$this->langFile::S1_P2.'</p>'.
+            '</section>'.
+            '<section>'.
+            '<p>'.$this->langFile::S2_P1.
+            '<ul>'.
+            '<li>'.$this->langFile::S2_P1_LI1.'</li>'.
+            '<li>'.$this->langFile::S2_P1_LI2.'</li>'.
+            '</ul>'.
+            '</p>'.
+            '</section>'.
+            '<section>'.
+            '<div class="note warning">'.$this->langFile::RJ_WARNING.'</div>'.
+            '</section>'.
+            '<section>'.
+            '<p>'.
+            $this->langFile::PR_TEXT_BEFORE.' '.
+            '<a href="'.$this->langSl.'/privacy" title="'.$this->langFile::PR_REF_TITLE.'">'.$this->langFile::PR_REF_TEXT.'</a>'.
+            '</p>'.
+            '</section>';
+    }
 }

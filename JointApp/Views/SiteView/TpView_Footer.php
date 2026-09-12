b6db2f622dc983f0b6f70356fc135b993eedf07d
Alexey <rightjoint@yandex.ru>
2026-09-12 20:07:50 +0300

    display informer

--- JointApp/Views/SiteView/TpView_Footer.php
+++ JointApp/Views/SiteView/TpView_Footer.php
@@ -35,9 +35,25 @@ class TpView_Footer extends TpView
         }
         //metrika
         else{
-            $pageFooter.='<img src="/img/y_metrika.png" style="height: 2em; width: auto; border:0; max-height: 31px; max-width: 88px;" '.
+            $pageFooter.='<a href="https://metrika.yandex.ru/stat/?id=44136454&amp;from=informer" target="_blank" rel="nofollow">'.
+                '<img src="https://informer.yandex.ru/informer/44136454/3_1_FFFFFFFF_EFEFEFFF_0_pageviews" '.
+                'style="width:auto; height:2em; border:0; max-height: 31px; max-width: 88px;" '.
                 'alt="'.$this->langFile::METRIC_ALT.'" title="'.$this->langFile::METRIC_TITLE.'" '.
-                'class="ym-advanced-informer" data-cid="44136454" data-lang="'.$this->langFile::LANG_LW.'" />';
+                'class="ym-advanced-informer" data-cid="44136454" data-lang="'.$this->langFile::LANG_LW.'" /></a>'.
+                '<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(44136454, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>'.
+                '<noscript><div><img src="https://mc.yandex.ru/watch/44136454" style="position:absolute; left:-9999px;" alt="" /></div></noscript>';
         }
 
         $buttons = '<img src="/img/footer-cats.png" style="height: 2em; width: auto; border:0; max-height: 31px; max-width: 88px;" '.
